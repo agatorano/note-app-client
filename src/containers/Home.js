65cab739b9f11a32824d41adc6f05148ac3d78a0
agatorano <agatorano@gmail.com>
2020-01-15 17:01:01 -0600

    Updating lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { API } from "aws-amplify";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 import "./Home.css";
 
 export default function Home(props) {
@@ -52,13 +53,21 @@ export default function Home(props) {
     }
 
   function renderLander() {
-    return (
-      <div className="lander">
-        <h1>Scratch</h1>
-        <p>A simple note taking app</p>
+  return (
+    <div className="lander">
+      <h1>Scratch</h1>
+      <p>A simple note taking app</p>
+      <div>
+        <Link to="/login" className="btn btn-info btn-lg">
+          Login
+        </Link>
+        <Link to="/signup" className="btn btn-success btn-lg">
+          Signup
+        </Link>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
   function renderNotes() {
     return (
